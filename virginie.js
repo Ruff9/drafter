7739514d01a22554c51571b2c977f43a773b2f3d
remy <remy.maucourt@yahoo.fr>
2020-02-07 11:10:12 +0100

    save content in browser local storage

--- virginie.js
+++ virginie.js
@@ -1,1 +1,15 @@
-console.log('youpi');
+var text = document.querySelector('.text');
+
+window.addEventListener('load', () => {
+    const gettingItem = window.localStorage.getItem('virginieContent');
+
+    if (gettingItem && gettingItem != "") {
+        text.innerHTML = gettingItem;
+    } else {
+        text.innerHTML = "<p>Écrire ici</p>"
+    }
+});
+
+document.addEventListener('keyup', (event) => {
+    window.localStorage.setItem('virginieContent', text.innerHTML);
+});
